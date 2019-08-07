2ac8a45dec18ed404a04eabd1feea08f9c5dda18
miki31 <it.misha2016@gmail.com>
2019-08-07 22:54:45 +0300

    user_table.sql

--- src/main/java/com/petro/apiary/domain/controller/IndexController.java
+++ src/main/java/com/petro/apiary/domain/controller/IndexController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("/")
 public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "Rest Service is now available    " +
-                "IT`S WORKING test test";
+        String datatime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd - MMMM - yyyy -- HH:mm:ss"));
+        return "Rest Service is now available     " +
+                "IT`S WORKING. Now is : ___ " +
+                datatime;
     }
 }
