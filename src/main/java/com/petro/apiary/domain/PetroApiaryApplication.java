6aba2ea958f5d9251d5239b2da7e58430fbc132a
miki31 <it.misha2016@gmail.com>
2019-08-20 00:45:46 +0300

    test security

--- src/main/java/com/petro/apiary/domain/PetroApiaryApplication.java
+++ src/main/java/com/petro/apiary/domain/PetroApiaryApplication.java
@@ -47,7 +47,7 @@ public class PetroApiaryApplication {
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8082);
+        connector.setPort(8080);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
